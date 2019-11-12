5f6b00ccf863fcd268ce84c34855b36f03ebd1e8
Julia Stachowiak <jstachowiak@egnyte.com>
2019-11-12 10:03:16 +0100

    refactor input in progress, lot of outs for debugging

--- jobshop/main.cpp
+++ jobshop/main.cpp
@@ -4,11 +4,10 @@
 #include <fstream>
 #include <string>
 
-void read_t(std::string filename);
-void read_b(std::string filename);
-void write_to_file(int** maszyny, int** czasy, std::string filename);
+void read_t(std::string filename, int** maszyny,  int** czasy, int* machinesCount, int* jobsCount);
+void read_b(std::string filename, int** maszyny,  int** czasy, int* machinesCount, int* jobsCount);
+void write_to_file(std::string filename, int** maszyny,  int** czasy, int* machinesCount, int* jobsCount);
 
-int main(int argc, char *argv[])
 // TODO: add argument to limit number of instances to solve from file
 /*
 argv:
@@ -17,237 +16,176 @@ argv:
 		t - Taillard's format
 		b - Beasley's format (orlib)
 */
+int main(int argc, char *argv[])
 {
 	std::ios_base::sync_with_stdio(false);
-	std::ifstream odczyt;
-	std::ofstream zapis;
-	int machinesCount;
-	int jobsCount;
-	int mCount = 0;
-	int jCount = 0;
 	std::string inFileName = argv[1];
 	std::string fileType = argv[2];
-	std::string line;
+
+	int** maszyny;
+	int** czasy;
+	int machinesCount, jobsCount;
 
 	std::cout << "filename: " << inFileName << ", type: " << fileType << '\n';
 
-	odczyt.open(inFileName, std::ios::in);
-	if (fileType == "t")
-		read_t();
-
-	else if (fileType == "b")
+	if (fileType[0] == 't')
 	{
-		if (odczyt.good())
-		{
-			std::cout << "brawo" << '\n';
+	    read_t(inFileName, maszyny, czasy, &machinesCount, &jobsCount);
+	    write_to_file("wynik_t.txt", maszyny, czasy, &machinesCount, &jobsCount);
+	}
 
-			do
-			{
-				std::getline(odczyt, line);
-			} while (line[0] != ' ' and line[1] != '+');
-			std::getline(odczyt, line);
-			odczyt >> jobsCount >> machinesCount;
-
-			int** maszyny = new int*[jobsCount];
-			int** czasy = new int*[jobsCount];
-
-			/*
-			odczyt >> machinesCount;
-			odczyt >> jobsCount;
-
-			int** maszyny = new int* [machinesCount];
-			for (int i = 0; i < jobsCount; i++)
-			{
-				maszyny[i] = new int[machinesCount];
-			}
-
-			int** czasy = new int* [jobsCount];
-			for (int i = 0; i < jobsCount; i++)
-			{
-				czasy[i] = new int[machinesCount];
-			}
-
-			for (int i = 0; i < jobsCount; i++)
-			{
-				for (int j = 0; j < machinesCount; j++)
-				{
-					odczyt >> czasy[i][j];
-				}
-			}
-
-			for (int i = 0; i < jobsCount; i++)
-			{
-				for (int j = 0; j < machinesCount; j++)
-				{
-					odczyt >> maszyny[i][j];
-				}
-			}
-
-			*/
-			odczyt.close();
-
-			zapis.open("wynik1.txt", std::ios::out);
-			if (zapis.good())
-			{
-				zapis << jobsCount << " ";
-				zapis << machinesCount << "\n";
-				for (int i = 0; i < jobsCount; i++)
-				{
-					for (int j = 0; j < machinesCount; j++)
-					{
-						zapis << czasy[i][j] << " ";
-					}
-					zapis << "\n";
-				}
-				for (int i = 0; i < jobsCount; i++)
-				{
-					for (int j = 0; j < machinesCount; j++)
-					{
-						zapis << maszyny[i][j] << " ";
-					}
-					zapis << "\n";
-				}
-				zapis.close();
-			}
-		}
+	else if (fileType[0] == 'b')
+	{
+		read_b(inFileName, maszyny, czasy, &machinesCount, &jobsCount);
+        // write_to_file("wynik_b.txt", maszyny, czasy, &machinesCount, &jobsCount);
 	}
 	else
-	{
-		std::cout << "blad\n";
-	}
+	    std::cout << "blad\n";
+
 	return 0;
-
 }
 
-void read_b(std::string filename)
+void read_b(std::string filename, int** maszyny,  int** czasy, int* machinesCount, int* jobsCount)
 {
 	std::ifstream odczyt;
-	std::ofstream zapis;
-	int machinesCount;
-	int jobsCount;
 	std::string line;
 
-
-	std::ifstream odczyt;
 	odczyt.open(filename, std::ios::in);
+	if(odczyt.good())
+	{
+	    std::cout << "Udalo sie otworzyc" << '\n';
 
+		odczyt >> (*jobsCount) >> (*machinesCount);
+		// std::getline(odczyt, line);
+		// std::getline(odczyt, line);
 
+		maszyny = new int*[*jobsCount];
+		czasy = new int*[*jobsCount];
+		for (int i = 0; i < (*jobsCount); ++i)
+		{
+			maszyny[i] = new int[*machinesCount];
+			czasy[i] = new int[*machinesCount];
+		}
+	}
+
+    for (int i = 0; i < (*jobsCount); ++i)
+    {
+        for (int j = 0; j < 2 * (*machinesCount); j+=2) {
+            odczyt >> maszyny[i][j/2] >> czasy[i][j/2];
+            std::cout << maszyny[i][j/2] << "\t" << czasy[i][j/2] << "\t";
+        }
+        std::cout<<"\n";
+    }
+    std::cout << "\n\n\n\n";
 
 }
 
-void read_t(std::string filename)
+void read_t(std::string filename, int** maszyny,  int** czasy, int* machinesCount, int* jobsCount)
 {
 	std::ifstream odczyt;
-	std::ofstream zapis;
-	int machinesCount;
-	int jobsCount;
 	std::string line;
 
-
-	std::ifstream odczyt;
 	odczyt.open(filename, std::ios::in);
 	if (odczyt.good())
 	{
 		std::cout << "Udalo sie otworzyc" << '\n';
 
-		odczyt >> jobsCount >> machinesCount;
+		odczyt >> (*jobsCount) >> (*machinesCount);
+		/*
 		std::getline(odczyt, line);
+		std::cout << line << '\n';
 		std::getline(odczyt, line);
+		std::cout << line<<"\n\n";
+		*/
 
-		int** maszyny = new int*[jobsCount];
-		int** czasy = new int*[jobsCount];
-		for (int i = 0; i < jobsCount; ++i)
+		maszyny = new int*[*jobsCount];
+		czasy = new int*[*jobsCount];
+		for (int i = 0; i < (*jobsCount); ++i)
 		{
-			maszyny[i] = new int[machinesCount];
-			czasy[i] = new int[machinesCount];
+			maszyny[i] = new int[*machinesCount];
+			czasy[i] = new int[*machinesCount];
 		}
 
 		// read times
-		for (int i = 0; i < jobsCount; ++i)
-			for (int j = 0; j < machinesCount; ++j)
-				odczyt >> czasy[i][j];
-		std::getline(odczyt, line);
+		for (int i = 0; i < (*jobsCount); ++i)
+			for (int j = 0; j < (*machinesCount); ++j)
+			{
+			    odczyt >> czasy[i][j];
+			    std::cout << czasy[i][j] << '\t';
+			}
+
+		// std::getline(odczyt, line);
+		// std::cout << line<<"\n\n";
 		// read machines
 		int x;
-		for (int i = 0; i < jobsCount; ++i)
-			for (int j = 0; j < machinesCount; ++j)
+		for (int i = 0; i < (*jobsCount); ++i)
+		 {
+			for (int j = 0; j < (*machinesCount); ++j)
 			{
 				odczyt >> x;
 				maszyny[i][j] = x;
+				std::cout << maszyny[i][j] << "\t";
 			}
-		// odczyt >> maszyny[i][j];
-
-		/*
-		odczyt >> machinesCount;
-		odczyt >> jobsCount;
-
-		int** maszyny = new int* [jobsCount];
-		for (int i = 0; i < jobsCount; i++)
-		{
-			maszyny[i] = new int[machinesCount];
+			std::cout << '\n';
 		}
-
-		int** czasy = new int* [jobsCount];
-		for (int i = 0; i < jobsCount; i++)
-		{
-			czasy[i] = new int[machinesCount];
-		}
-
-		for (int i = 0; i < jobsCount; i++)
-		{
-			for (int j = 0; j < (2 * machinesCount); j++)
-			{
-				if (j % 2 == 0)
-				{
-					odczyt >> maszyny[i][mCount];
-					mCount++;
-				}
-				else
-				{
-					odczyt >> czasy[i][jCount];
-					jCount++;
-				}
-			}
-			mCount = 0;
-			jCount = 0;
-		}
-		*/
 		odczyt.close();
 	}
 }
 
-void write_to_file(int** maszyny, int** czasy, std::string filename)
+void write_to_file(std::string filename, int** maszyny,  int** czasy, int* machinesCount, int* jobsCount)
 {
+    std::cout << "machines count " << (*machinesCount) << '\n' << "jobs count " << (*jobsCount) << '\n';
+    for(int i=0;i<(*jobsCount);++i)
+    {
+        for(int j=0;j<(*machinesCount);++j)
+            std::cout << maszyny[i][j] << "\t";
+        std::cout<<'\n';
+    }
+    std::cout << '\n';
+    for(int i=0;i<(*jobsCount);++i)
+    {
+        for(int j=0;j<(*machinesCount);++j)
+            std::cout << czasy[i][j] << "\t";
+        std::cout<<'\n';
+    }
+
+
+    /*
 	std::ofstream zapis;
-	int machinesCount;
-	int jobsCount;
 	zapis.open(filename, std::ios::out);
 
 	if (zapis.good())
-	std::cout << "Maszyny:" << '\n';
-	for (int i = 0; i < jobsCount; ++i)
 	{
-		for (int j = 0; j < machinesCount; ++j)
-		{
-			std::cout << maszyny[i][j] << "\t";
-			zapis << maszyny[i][j] << "\t";
-		}
-		std::cout << '\n';
-		zapis << '\n';
-	}
-	std::cout << "Czasy:" << '\n';
-	for (int i = 0; i < jobsCount; i++)
-	{
-		for (int j = 0; j < machinesCount; j++)
-		{
-			std::cout << czasy[i][j] << "\t";
-			zapis << czasy[i][j] << "\t";
-		}
-		std::cout << '\n';
-		zapis << '\n';
-	}
-	std::cout << "machines count " << machinesCount << '\n' << "jobs count " << jobsCount << '\n';
-	zapis << "machines count " << machinesCount << '\n' << "jobs count " << jobsCount << '\n';
+	    std::cout << "machines count " << (*machinesCount) << '\n' << "jobs count " << (*jobsCount) << '\n';
+        // zapis << "machines count " << (*machinesCount) << '\n' << "jobs count " << (*jobsCount) << '\n';
+        std::cout << "Maszyny:\n";
+        std::cout << "1";
+        for (int i = 0; i < (*jobsCount); ++i)
+        {
+            std::cout << "2";
+            for (int j = 0; j < (*machinesCount); ++j)
+            {
+                std::cout << maszyny[i][j] << "\t";
+                zapis << maszyny[i][j] << "\t";
+            }
+            std::cout << '\n';
+            zapis << '\n';
+        }
+        std::cout << "Czasy:\n";
+        for (int i = 0; i < (*jobsCount); i++)
+        {
+            for (int j = 0; j < (*machinesCount); j++)
+            {
+                std::cout << czasy[i][j] << "\t";
+                zapis << czasy[i][j] << "\t";
+            }
+            std::cout << '\n';
+            zapis << '\n';
+        }
+        std::cout << "machines count " << (*machinesCount) << '\n' << "jobs count " << (*jobsCount) << '\n';
+        zapis << "machines count " << (*machinesCount) << '\n' << "jobs count " << (*jobsCount) << '\n';
 
-	zapis.close();
+        zapis.close();
+    }
+    */
 }
